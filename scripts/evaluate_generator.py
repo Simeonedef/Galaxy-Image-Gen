0ebea33b3bf92bcb95435f1a4110a73e8f67ac4b
Carlos Gomes <carlosmiguel.gomes@live.com.pt>
2020-07-19 17:20:54 +0100

    Add functionality to save images above a certain score threshold

--- scripts/evaluate_generator.py
+++ scripts/evaluate_generator.py
@@ -10,6 +10,7 @@ from generators.two_stage_combined_model import TwoStageCombinedModel
 from regressors.RESNET import ResnetRegressor
 from regressors.random_forest import RandomForestRegressor
 from generators.full_galaxy_gan_generator import FullGalaxyGan
+from PIL import Image
 import numpy as np
 
 class DummyRegressor:
@@ -59,10 +60,25 @@ def get_regressor(args):
     else:
         raise Exception("model does not exist")
 
+def save_images(images, scores, threshold, name):
+    indices_to_save = scores >= threshold
+    scores_to_save = scores[indices_to_save]
+    images_to_save = [images[i] for i in range(len(images)) if indices_to_save[i]]
+    # images_to_save = [img.reshape(1, 1000, 1000) for img in images_to_save]
+    save_dir = "../data/saved_images_" + name
+    if os.path.exists(save_dir):
+        print("WARNING: directory '{}' already exists. Please delete if you want to overwrite.".format(save_dir))
+        return
+    os.mkdir(save_dir)
+    for index, image in enumerate(images_to_save):
+        Image.fromarray(image.astype(np.uint8)).save(os.path.join(save_dir, str(scores_to_save[index])[:3] + "_"+str(index))+".png", "PNG")
 
-def evaluate(generator, regressor, n_images, visualize=False):
+
+def evaluate(generator, regressor, n_images, visualize=False, save_score_threshold=-1, name=""):
     images = generator.generate(n_images)
     scores = np.asarray(regressor.score(images))
+    if (save_score_threshold != -1):
+        save_images(images, scores, save_score_threshold, name)
     if visualize:
         assert n_images >= 2
 
@@ -85,7 +101,6 @@ def evaluate(generator, regressor, n_images, visualize=False):
     print("Top 10 scores: ", scores[np.argsort(scores)][-10:])
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Evaluates a given generator model using a regressor model.')
     # general script arguments
@@ -99,6 +114,7 @@ if __name__ == "__main__":
                                                           'full_galaxy_gan'], default='baseline', help='name of the generator to evaluate')
     parser.add_argument('--regressor', type=str, choices=['resnet', 'rf', 'dummy'], default='resnet', help='name of the regressor that produces the scores for the generator')
     parser.add_argument('--n_images', type=int, default=16, help='number of images to evaluate on')
+    parser.add_argument('--save_images', type=int, default=-1, help='save images with score greater than this, -1 dont save')
     parser.add_argument('--visualize', action='store_true', help='if enabled displays images along with their score')
 
     # baseline models only arguments
@@ -111,4 +127,4 @@ if __name__ == "__main__":
     generator = get_generator(args)
     regressor = get_regressor(args)
 
-    evaluate(generator, regressor, args.n_images, visualize=args.visualize)
+    evaluate(generator, regressor, args.n_images, visualize=args.visualize, save_score_threshold=args.save_images, name=args.generator)
